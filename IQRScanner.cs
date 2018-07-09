2d079064ac02fe48d3c62f9ebbaf7806c8a9b0c6
Hakob23 <36648940+Hakob23@users.noreply.github.com>
2018-07-09 13:09:50 +0400

    Add files via upload

---
+++ IQRScanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Waiter_QR.Service
+{
+    public interface IQRScanner
+    {
+        Task<string> ScanAsync();
+    }
+}
