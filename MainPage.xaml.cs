a3d15f18cc76d84ac080bf262ffccd1485078845
Hakob23 <36648940+Hakob23@users.noreply.github.com>
2018-07-09 13:09:04 +0400

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Waiter_QR.Service;
+using Xamarin.Forms;
+
+
+namespace Waiter_QR
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQRScanner>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarcode.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
